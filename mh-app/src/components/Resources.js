00baebc0401086a3e1326ff2a66953dfae53a338
Osc-53215 <80727193+Osc-53215@users.noreply.github.com>
2021-06-26 21:28:47 -0500

    finished the resource page for now

--- mh-app/src/components/Resources.js
+++ mh-app/src/components/Resources.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import './Resources.css';
-import image from  './photos/image_1.png';
+import image1 from  './photos/image_1.png';
+import image2 from  './photos/image_2.png';
+import image3 from  './photos/image_3.png';
+import image4 from  './photos/image_4.png';
+import image5 from  './photos/image_5.png';
+import image6 from  './photos/image_6.png';
+import image7 from  './photos/image_7.png';
+import image8 from  './photos/image_8.png';
+import image9 from  './photos/image_9.png';
+import image10 from  './photos/image_10.png';
 
 
 function Resources() {
     return (
         <div className = 'post'>
 
-        <img className = 'post__image' src = {image} alt = ''/>
+        <img className = 'post__image' src = {image1} alt = ''/>
+        <img className = 'post__image' src = {image2} alt = ''/>
+        <img className = 'post__image' src = {image3} alt = ''/>
+        <img className = 'post__image' src = {image4} alt = ''/>
+        <img className = 'post__image' src = {image5} alt = ''/>
+        <img className = 'post__image' src = {image6} alt = ''/>
+        <img className = 'post__image' src = {image7} alt = ''/>
+        <img className = 'post__image' src = {image8} alt = ''/>
+        <img className = 'post__image' src = {image9} alt = ''/>
+        <img className = 'post__image' src = {image10} alt = ''/>
     </div>
     )
 }
