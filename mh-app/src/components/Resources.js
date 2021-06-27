dc81eaa25378f7474393fd04d81fc2bc29b62dee
Osc-53215 <80727193+Osc-53215@users.noreply.github.com>
2021-06-26 21:21:43 -0500

    successfully adde image now styling

--- mh-app/src/components/Resources.js
+++ mh-app/src/components/Resources.js
@@ -1,29 +1,13 @@
-import React from 'react'
-import './Resources.css'
+import React from 'react';
+import './Resources.css';
+import image from  './photos/image_1.png';
 
 
 function Resources() {
     return (
         <div className = 'post'>
 
-        <div className = ''>
-            <h1>Broader Support: </h1>
-            <h3>Social Support</h3>
-            <img src = 'file:///C:/Users/SpeQiu/Downloads/Resource%20Packet_For%20Website.pdf'/>
-            <p>Rise trains and hires community college and university students to organize
-                campaigns focused on eliminating tuition and fees, expanding financial aid and
-
-                ending student hunger and homelessness. Although Rise students work
-
-                nationwide, the primary focus states in 2020 were California, Georgia, Michigan,
-                Pennsylvania, and Wisconsin. Given the rising costs of education and the barrier
-                it places on underrepresented students when pursuing higher education, this
-                resource is useful in helping first-generation Latine students identify and advocate
-                for their needs. This resource embodies social justice by creating a team of people
-                prepared to address educational disparities.
-            </p>
-        </div>
-    
+        <img className = 'post__image' src = {image} alt = ''/>
     </div>
     )
 }
